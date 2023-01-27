15fa62e7617ed2e355b2bf40a072da9b7d44b8e6
FGiuliari <7384983+FGiuliari@users.noreply.github.com>
2023-01-27 14:59:08 +0100

    modified frankling scripts

--- singularity/run_wikiart.sh
+++ singularity/run_wikiart.sh
@@ -2,7 +2,7 @@
 cd /work/fgiuliari/PuzzleDiffusion-GNN/singularity
 
 NAME='Diff-GNN'
-ARGS='-gpus 2 -batch_size 24 -steps 400 -num_workers 10'
+ARGS='-gpus 3 -batch_size 8 -steps 700 -num_workers 8'
 
 
 qsub -v pyfile=puzzle_diff/train_wikiart.py,args="$ARGS" -N "$NAME"  pbs_args.sh

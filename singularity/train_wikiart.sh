24e6e8bc78d6e7ba387533db8f2b6094d3069db5
7384983+FGiuliari@users.noreply.github.com <7384983+FGiuliari@users.noreply.github.com>
2023-02-01 13:44:37 +0100

    bugfix

--- singularity/train_wikiart.sh
+++ singularity/train_wikiart.sh
@@ -15,5 +15,5 @@ NAME='Diff $dataset $puzzles $steps'
 ARGS='-dataset $dataset -puzzle_sizes $puzzles -inference_ratio $inference_ratio -sampling $sampling -gpus $gpus -batch_size $batch_size -steps $steps -num_workers $cpus '
 
 
-qsub -l :ngpus=$gpus:ncpus=$cpus -v pyfile=puzzle_diff/train_wikiart.py,args="$ARGS" -N "$NAME"  pbs_args.sh
+qsub -l :ngpus=$gpus:ncpus=$cpus -v pyfile=puzzle_diff/train_script.py,args="$ARGS" -N "$NAME"  pbs_args.sh
 
